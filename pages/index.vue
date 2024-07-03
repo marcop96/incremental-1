010d6730bea0523007403fef20c6d58f9026d039
Marco Pianaroli <marco.pianaroli96@gmail.com>
2024-07-03 15:02:46 +0200

    feat: adds shadcn-vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,26 @@
 <script setup lang="ts">
-  import { useSkillStore } from "../composable/useSkills";
-  import { storeToRefs } from 'pinia';
-  import TheSkills from '../components/TheSkills.vue';
+import { storeToRefs } from 'pinia'
+import { useSkillStore } from '../composable/useSkills'
+import TheSkills from '../components/TheSkills.vue'
+import { Progress } from '@/components/ui/Progress'
 
-  const skillStore = useSkillStore();
-  const { skills, activeSkill, changeActiveSkill } = storeToRefs(skillStore);
+const skillStore = useSkillStore()
+const { skills, activeSkill, changeActiveSkill } = storeToRefs(skillStore)
 </script>
 
 <template>
   <div class="flex justify-center items-center p-3">
-    <h2 v-if="activeSkill !== 'inventory' && activeSkill !== 'skills'" class="text-2xl">{{ activeSkill.name }}</h2>
+    <h2
+      v-if="activeSkill !== 'inventory' && activeSkill !== 'skills'"
+      class="text-2xl"
+    >
+      {{ activeSkill.name }}
+    </h2>
   </div>
 
   <!-- <div v-if="activeSkill =='inventory'"></div> -->
-  <TheSkills v-if="activeSkill" :skill="activeSkill" />
-
+  <TheSkills
+    v-if="activeSkill"
+    :skill="activeSkill"
+  />
 </template>
