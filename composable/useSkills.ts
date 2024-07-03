0dd6dc54ea11cba81729c037e8d662e2b559e686
Marco Pianaroli <marco.pianaroli96@gmail.com>
2024-07-03 10:54:09 +0200

    feat: creates skill store and displays skills on default layout

---
+++ composable/useSkills.ts
@@ -0,0 +1,18 @@
+import { defineStore } from 'pinia'
+import type { Skill } from '@/types'
+
+const activeSkill = ref<string>('woodcutting')
+export const useSkillStore = defineStore('skills', () => {
+  const skills = ref<Skill[]>([
+    { id: 1, name: 'woodcutting' },
+    { id: 2, name: 'firemaking' },
+    { id: 3, name: 'fishing' },
+    { id: 4, name: 'cooking' },
+  ])
+
+  const changeActiveSkill = (name: string) => {
+    activeSkill.value = name
+    console.log('Active skill changed to:', name)
+  }
+  return { skills, activeSkill, changeActiveSkill }
+})
---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import { useSkillStore } from "../composable/useSkills";
+  import { storeToRefs } from 'pinia';
+
+  const skillStore = useSkillStore();
+  const { skills, activeSkill, changeActiveSkill } = storeToRefs(skillStore);
+</script>
+
+<template>
+  <div class="flex">
+    <div class="h-screen w-1/5 bg-green-400 bg-opacity-75 p-4">
+      <h2 class="text-2xl font-bold">Inventory</h2>
+      <h2 class="text-2xl font-bold">Skills</h2>
+      <ul>
+        <li v-for="skill in skills" :key="skill.id">
+          <button @click="skillStore.changeActiveSkill(skill.name)">{{ skill.name }}</button>
+        </li>
+      </ul>
+    </div>
+    <div class="flex-1">
+      <NuxtPage />
+    </div>
+  </div>
+</template>
