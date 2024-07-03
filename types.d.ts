16154223d452c01538b0764f05dcf23bd0507265
Marco Pianaroli <marco.pianaroli96@gmail.com>
2024-07-03 11:48:36 +0200

    feat: creates resources type, and adds some woodcutting resources

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,13 @@
 export interface Skill {
   id: number
   name: string
+}
 
+export interface Resource {
+  id: number
+  name: string
+  skillId: number
+  requiredLevel: number
+  experienceGiven: number
+  timeToGather: number
 }
