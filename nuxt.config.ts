a8be144ee312e75afa1a3ac4d2aec83c501cca52
Marco Pianaroli <marco.pianaroli96@gmail.com>
2024-07-03 10:54:42 +0200

    chore: updates app.vue and adds modules to nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"],
+  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", '@pinia/nuxt'],
+
   eslint: {
     config:{
       stylistic: true,
     }
-  }
+  },
+
+  compatibilityDate: "2024-07-03"
 })
