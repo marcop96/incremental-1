de6ecf93610c308bee5fe0156b10593fc89a1d85
Marco Pianaroli <marco.pianaroli96@gmail.com>
2024-07-03 10:11:32 +0200

    initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,10 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss"],
+  eslint: {
+    config:{
+      stylistic: true,
+    }
+  }
+})
