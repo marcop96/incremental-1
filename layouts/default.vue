b8e558427556e7c3cbf4c714246a4b02112d2c64
Marco Pianaroli <marco.pianaroli96@gmail.com>
2024-07-04 09:23:50 +0200

    chore: removes unused variables

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,21 +1,35 @@
 <script setup lang="ts">
-  import { useSkillStore } from "../composable/useSkills";
-  import { storeToRefs } from 'pinia';
+import { storeToRefs } from 'pinia'
+import { useSkillStore } from '../composable/useSkills'
 
-  const skillStore = useSkillStore();
-  const { skills, activeSkill, changeActiveSkill } = storeToRefs(skillStore);
+const skillStore = useSkillStore()
+const { skills } = storeToRefs(skillStore)
 </script>
 
 <template>
   <div class="flex">
     <div class="h-screen w-1/5 bg-green-400 bg-opacity-75 p-4">
       <ul>
-        <li class='flex flex-col'>
-        <button @click="skillStore.changeActiveSkill('inventory')" class="text-2xl font-bold">Inventory</button>
-        <button @click="skillStore.changeActiveSkill('skills')" class="text-2xl font-bold">Skills</button>
+        <li class="flex flex-col">
+          <button
+            class="text-2xl font-bold"
+            @click="skillStore.changeActiveSkill('inventory')"
+          >
+            Inventory
+          </button>
+          <button
+            class="text-2xl font-bold"
+          >
+            Skills
+          </button>
         </li>
-        <li v-if="activeSkill !== 'inventory'" v-for="skill in skills" :key="skill.id">
-          <button @click="skillStore.changeActiveSkill(skill)">{{ skill.name }}</button>
+        <li
+          v-for="skill in skills"
+          :key="skill.id"
+        >
+          <button @click="skillStore.changeActiveSkill(skill)">
+            {{ skill.name }}
+          </button>
         </li>
       </ul>
     </div>
