58f0c8f9a24bafc96871deda5a7da18175b48ffa
Marco <me@marcopianaroli.com>
2024-07-08 19:10:05 +0200

    deps: adds nuxt icon module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxtjs/tailwindcss", '@pinia/nuxt', "shadcn-nuxt"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxtjs/tailwindcss",
+    '@pinia/nuxt',
+    "shadcn-nuxt",
+    "@nuxt/icon"
+  ],
 
   eslint: {
     config:{
