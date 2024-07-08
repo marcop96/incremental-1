ed98115e16fe0981beb59cdd1bdecbf43d8517db
Marco <me@marcopianaroli.com>
2024-07-08 19:33:29 +0200

    style:adds hover element

--- components/TheInventory.vue
+++ components/TheInventory.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
-
 import { useInventoryStore } from '../composable/useInventory'
 import { useSkillStore } from '~/composable/useSkills'
+import ItemCard from '@/components/ItemCard.vue'
 
 const inventoryStore = useInventoryStore()
 const skillStore = useSkillStore()
-
 const { inventory } = storeToRefs(inventoryStore)
+
+// const itemiconplaceholder = 'https://via.placeholder.com/400x200'
 </script>
 
 <template>
@@ -23,12 +24,11 @@ const { inventory } = storeToRefs(inventoryStore)
   <h2 class="text-xl font-bold">
     Inventory
   </h2>
-  <ul>
-    <li
+  <section class="grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+    <ItemCard
       v-for="item in inventory"
       :key="item.id"
-    >
-      {{ item.name }} - {{ item.quantity }}
-    </li>
-  </ul>
+      :item="item"
+    />
+  </section>
 </template>
