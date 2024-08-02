4067cc4a9682e52feceb075b5c6620a4072e1296
Marco Pianaroli <marco.pianaroli96@gmail.com>
2024-08-02 11:08:56 +0200

    feat: adds changePage logic, to swap between skills and inventory or shop

--- composable/useSkills.ts
+++ composable/useSkills.ts
@@ -2,7 +2,7 @@ import { defineStore } from 'pinia'
 import type { Skill } from '@/types'
 
 const activeSkill = ref<Skill | null>(null)
-
+const activePage = ref<'inventory' | 'shop' | null>(null)
 export const useSkillStore = defineStore('skills', () => {
   const skills = ref<Skill[]>([
     { id: 1, name: 'woodcutting', level: 1, xp: 0, isGathering: true },
@@ -14,5 +14,8 @@ export const useSkillStore = defineStore('skills', () => {
   const changeActiveSkill = (skill: Skill | null) => {
     activeSkill.value = skill
   }
-  return { skills, activeSkill, changeActiveSkill }
+  const changeActivePage = (page: 'inventory' | 'shop' | null) => {
+    activePage.value = page
+  }
+  return { skills, activeSkill, changeActiveSkill, changeActivePage, activePage }
 })
